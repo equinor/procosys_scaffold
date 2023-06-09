3e474ceee8238cb0d46c63a71a0a60174b18d930
Jan Inge Dalsbø <JDALSB@equinor.com>
2023-06-09 15:17:47 +0200

    Migration ProjectDeletedInSource

--- src/Equinor.ProCoSys.PCS5.Infrastructure/Migrations/20230609131355_ProjectDeletedInSource.Designer.cs
+++ src/Equinor.ProCoSys.PCS5.Infrastructure/Migrations/20230609131355_ProjectDeletedInSource.Designer.cs
@@ -4,6 +4,7 @@ using Equinor.ProCoSys.PCS5.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Equinor.ProCoSys.PCS5.Infrastructure.Migrations
 {
     [DbContext(typeof(PCS5Context))]
-    partial class PCS5ContextModelSnapshot : ModelSnapshot
+    [Migration("20230609131355_ProjectDeletedInSource")]
+    partial class ProjectDeletedInSource
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
