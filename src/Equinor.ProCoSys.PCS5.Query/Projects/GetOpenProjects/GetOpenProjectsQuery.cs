54ebacbe38257c3e6b1eaf445c218e51baaf5d28
Jan Inge Dalsbø <JDALSB@equinor.com>
2023-06-12 08:32:51 +0200

    No need for GetOpenProjectsQuery. Use CacheController and get projects with access

--- src/Equinor.ProCoSys.PCS5.Query/Projects/GetOpenProjects/GetOpenProjectsQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using MediatR;
-using ServiceResult;
-
-namespace Equinor.ProCoSys.PCS5.Query.Projects.GetOpenProjects;
-
-public class GetOpenProjectsQuery : IRequest<Result<List<ProjectDto>>>
-{
-}
