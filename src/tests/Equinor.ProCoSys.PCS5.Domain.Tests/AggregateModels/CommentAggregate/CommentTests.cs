4fed48cb0b4da5fba9b4f31cd07980d0755c4017
Jan Inge Dalsbø <JDALSB@equinor.com>
2023-06-12 08:43:56 +0200

    More comments unit tests

--- src/tests/Equinor.ProCoSys.PCS5.Domain.Tests/AggregateModels/CommentAggregate/CommentTests.cs
+++ src/tests/Equinor.ProCoSys.PCS5.Domain.Tests/AggregateModels/CommentAggregate/CommentTests.cs
@@ -1,26 +1,24 @@
 ﻿using System;
-using Equinor.ProCoSys.PCS5.Domain.AggregateModels.LinkAggregate;
+using Equinor.ProCoSys.PCS5.Domain.AggregateModels.CommentAggregate;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Equinor.ProCoSys.PCS5.Domain.Tests.AggregateModels.LinkAggregate;
+namespace Equinor.ProCoSys.PCS5.Domain.Tests.AggregateModels.CommentAggregate;
 
 [TestClass]
-public class LinkTests
+public class CommentTests
 {
-    private Link _dut;
+    private Comment _dut;
     private readonly string _sourceType = "X";
     private readonly Guid _sourceGuid = Guid.NewGuid();
-    private readonly string _title = "A";
-    private readonly string _url = "Desc A";
+    private readonly string _text = "A";
 
     [TestInitialize]
-    public void Setup() => _dut = new Link(_sourceType, _sourceGuid, _title, _url);
+    public void Setup() => _dut = new Comment(_sourceType, _sourceGuid, _text);
 
     [TestMethod]
     public void Constructor_ShouldSetProperties()
     {
-        Assert.AreEqual(_title, _dut.Title);
-        Assert.AreEqual(_url, _dut.Url);
+        Assert.AreEqual(_text, _dut.Text);
         Assert.AreEqual(_sourceType, _dut.SourceType);
         Assert.AreEqual(_sourceGuid, _dut.SourceGuid);
         Assert.AreNotEqual(_sourceGuid, _dut.Guid);
